e608a59be9ac00f03ad424877e14ae97224606a8
fdervaux <francois.dervaux@gmail.com>
2024-01-19 14:00:03 +0100

    update Timer and penalty

--- Assets/Scripts/UI/ButtonController.cs
+++ Assets/Scripts/UI/ButtonController.cs
@@ -12,17 +12,48 @@ public class ButtonController : MonoBehaviour, IPointerDownHandler, IPointerUpHa
 {
     [SerializeField] private UnityEvent _onClick;
     [SerializeField] private Image _image;
-    private CanvasGroup _canvasgroup;   
+    [SerializeField] private Image _secondaryImage;
+
+    [SerializeField] private bool _changeColorOnPress = false;
+    [SerializeField] private Color _pressedColor = Color.white;
+    [SerializeField] private Color _normalColor = Color.white;
+
+
+    private CanvasGroup _canvasgroup;
 
     private bool _isActivated = true;
 
     public bool IsActivated { get => _isActivated; }
 
+
+    public void SwitchImage()
+    {
+        if (_image == null || _secondaryImage == null)
+            return;
+
+        if (_image.gameObject.activeSelf)
+        {
+            _image.gameObject.SetActive(false);
+            _secondaryImage.gameObject.SetActive(true);
+        }
+        else
+        {
+            _image.gameObject.SetActive(true);
+            _secondaryImage.gameObject.SetActive(false);
+        }
+    }
+
+    public UnityEvent OnClick
+    {
+        get => _onClick;
+        set => _onClick = value;
+    }
+
     public void Awake()
     {
         _canvasgroup = GetComponent<CanvasGroup>();
 
-        if(_canvasgroup == null)
+        if (_canvasgroup == null)
             return;
 
         _canvasgroup.alpha = 0.5f;
@@ -52,6 +83,18 @@ public class ButtonController : MonoBehaviour, IPointerDownHandler, IPointerUpHa
     public void OnPointerUp(PointerEventData eventData)
     {
         transform.DOScale(1f, 0.1f);
+
+        if (_changeColorOnPress)
+        {
+            Image[] images = GetComponentsInChildren<Image>();
+
+            foreach (Image image in images)
+            {
+                image.DOColor(_normalColor, 1f).From(_pressedColor).SetEase(Ease.OutCubic);
+            }
+
+        }
+
         _onClick.Invoke();
     }
 }
--- Assets/UnlockGameManager.cs
+++ Assets/UnlockGameManager.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
+using TMPro;
 using UnityEngine;
 
 public class UnlockGameManager : MonoBehaviour
@@ -12,9 +14,54 @@ public class UnlockGameManager : MonoBehaviour
     [SerializeField] private ButtonController _machineButton;
     [SerializeField] private ButtonController _reviewClueButton;
 
+    [Header("Timer")]
 
-    
+    [SerializeField] private TextMeshProUGUI _timerText;
+    [SerializeField] private RectTransform _timerPanel; // 30 seconds
+    [SerializeField] private float _startTimeTimer = 600; // 10 minutes
+    [SerializeField] private float _penaltyTime = 60; // 60 seconds
+    [SerializeField] private Color _penaltyTextColor = Color.red;
+    [SerializeField] private Color _normalTextColor = Color.black;
 
+
+
+    private bool _isPlaying = false;
+
+    private float _timeLeft = 0;
+
+    public float TimeLeft
+    {
+        get => _timeLeft;
+        set
+        {
+            _timeLeft = Mathf.Max(value, 0);
+        }
+    }
+
+    private void SetTime(float time)
+    {
+        TimeLeft = time;
+        UpdateTimerText();
+    }
+
+
+    private void UpdateTime(float deltaTime)
+    {
+        if (_isPlaying)
+        {
+            TimeLeft -= deltaTime;
+
+            UpdateTimerText();
+        }
+    }
+
+    private void UpdateTimerText()
+    {
+        int minutes = Mathf.FloorToInt(TimeLeft / 60);
+        int seconds = Mathf.FloorToInt(TimeLeft % 60);
+
+        _timerText.text = string.Format("{0}:{1}", minutes.ToString("00"), seconds.ToString("00"));
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -26,12 +73,51 @@ public class UnlockGameManager : MonoBehaviour
         _hiddenObjectButton.Deactivate();
         _machineButton.Deactivate();
         _reviewClueButton.Deactivate();
-        
+
+        _playPauseButton.OnClick.AddListener(PlayPauseButtonClick);
+        _penaltyButton.OnClick.AddListener(PenaltyButtonClick);
+
+        SetTime(_startTimeTimer);
+
+        _timerText.color = _normalTextColor;
+    }
+
+    void PenaltyButtonClick()
+    {
+        SetTime(TimeLeft - _penaltyTime);
+
+        _timerText.DOColor(_normalTextColor, 2f).From(_penaltyTextColor).SetEase(Ease.OutCubic);
+        _timerPanel.DOShakeAnchorPos(0.5f, 20, 100, 90, false, true).SetEase(Ease.OutCubic);
+    }
+
+
+    private void PlayPauseButtonClick()
+    {
+        _isPlaying = !_isPlaying;
+
+        if (_isPlaying)
+        {
+            _playPauseButton.SwitchImage();
+            _penaltyButton.Activate();
+            _clueButton.Activate();
+            _codeButton.Activate();
+            _machineButton.Activate();
+        }
+
+        else
+        {
+            _playPauseButton.SwitchImage();
+            _penaltyButton.Deactivate();
+            _clueButton.Deactivate();
+            _codeButton.Deactivate();
+            _machineButton.Deactivate();
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        UpdateTime(Time.deltaTime);
     }
 }
