a2a7002f297f4ba081cca26bbc2910b1d219ca14
fdervaux <francois.dervaux@gmail.com>
2024-02-05 23:22:18 +0100

    update

---
+++ Assets/Promotion.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class Promotion
+{
+    public string PromotionName;
+    public Sprite image;
+
+    public List<UnlockGame> games;
+
+}
