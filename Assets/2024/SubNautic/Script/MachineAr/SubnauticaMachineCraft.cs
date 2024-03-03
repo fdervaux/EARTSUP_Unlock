0e6a57aba09367c34cd2c1bb7aa9d38c1067b9e1
Arthur Caumartin <artcaumart@gmail.com>
2024-03-03 18:07:27 +0100

    Add Craft Machine

---
+++ Assets/2024/SubNautic/Script/MachineAr/SubnauticaMachineCraft.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+[Serializable]
+public class Craft
+{
+    public string itemName;
+    public Image itemImage;
+    public Image targetAR;
+    [Space]
+    public string eventName;
+}
+
+public class SubnauticaMachineCraft : MonoBehaviour
+{
+    [SerializeField] private GameObject _buttonPanel;
+    [SerializeField] private GameObject _selectedCraftPanel;
+    [Space]
+    [SerializeField] private TextMeshProUGUI _textItemName;
+    [SerializeField] private Image _selectedCraftItemImage;
+    [SerializeField] private Image _selectedCraftTargetAR;
+    private Craft _currentCraft;
+    private Coroutine _valideCraft;
+
+    void Start()
+    {
+        Return();
+    }
+
+    public void OnRecipeFind()
+    {
+        if(_currentCraft != null)
+            UnlockGameManager.Instance.TriggerEvent(_currentCraft.eventName);
+    }
+
+    public void SelectCraft(Craft craft)
+    {
+        _currentCraft = craft;
+
+        _buttonPanel.SetActive(false);
+        _textItemName.text = _currentCraft.itemName;
+        _selectedCraftItemImage = _currentCraft.itemImage;
+        _selectedCraftTargetAR = _currentCraft.targetAR;
+        _selectedCraftPanel.SetActive(true);
+
+        _valideCraft = StartCoroutine(DelayCraftValidation());
+    }
+
+    public void Return()
+    {
+        if(_valideCraft != null)
+            StopCoroutine(_valideCraft);
+
+        _selectedCraftPanel.SetActive(false);
+        _textItemName.text = "";
+        _selectedCraftItemImage = null;
+        _selectedCraftTargetAR = null;
+        _buttonPanel.SetActive(true);
+    }
+
+    private IEnumerator DelayCraftValidation()
+    {
+        yield return new WaitForSeconds(2f);
+        OnRecipeFind();
+    }
+}
