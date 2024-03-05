101ae6a3d19c0be1a0efded9032b8d7999fe23a1
fdervaux <francois.dervaux@gmail.com>
2024-03-05 09:50:00 +0100

    update

--- Assets/Unlock_base/Scripts/Data/Hint.cs
+++ Assets/Unlock_base/Scripts/Data/Hint.cs
@@ -3,8 +3,8 @@ using UnityEngine.Localization;
 [System.Serializable]
 public class Hint : IUnlockSaveHint
 {
-    public bool HasMoreHint;
-    public bool HasSolution;
+    //zpublic bool HasMoreHint;
+    //public bool HasSolution;
     public LocalizedString hintMessage1;
     public LocalizedString hintMessage2;
     public LocalizedString hintMessageAnswer;
