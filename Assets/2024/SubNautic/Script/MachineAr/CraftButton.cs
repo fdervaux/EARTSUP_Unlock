6d7d904e5ff1e5453af1e7878a8dd3dd83da837c
Arthur Caumartin <artcaumart@gmail.com>
2024-03-17 11:31:57 +0100

    Finish craft machine

--- Assets/2024/SubNautic/Script/MachineAr/CraftButton.cs
+++ Assets/2024/SubNautic/Script/MachineAr/CraftButton.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class CraftButton : MonoBehaviour
 {
     [SerializeField] private SubnauticaMachineCraft _machineCraft;
     [SerializeField] private Craft _craft;
+    [SerializeField] private string _caftIndex;
 
     [ContextMenu("Start fct")]
     void Start()
@@ -16,6 +19,10 @@ public class CraftButton : MonoBehaviour
 
     public void OnClic()
     {
-        _machineCraft.SelectCraft(_craft);
+        // _machineCraft.SelectCraft(_craft);
+        if(_caftIndex == "")
+            return;
+        UnlockGameManager.Instance.TriggerEvent(_caftIndex);
+        UnlockGameManager.Instance.ShowMenu(() => { SceneManager.UnloadSceneAsync("Subnautica_Labyrinth"); });
     }
 }
